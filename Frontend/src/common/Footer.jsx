9636452f8d3146959cfce183337afc587995d7d0
AmandeepRawat <Ar464163@gmail.com>
2026-05-08 13:21:21 +0530

    footer

--- Frontend/src/common/Footer.jsx
+++ Frontend/src/common/Footer.jsx
@@ -9,16 +9,16 @@ const Footer = () => {
         navigate('/signup')
     }
   return (
-    <div className=" p-2 w-full ">
+    <div className=" p-3 md:p-2 w-full ">
       <div className=' h-18 flex flex-row justify-between items-center rounded-lg bg-[linear-gradient(to_right,#A237A2,#865BC0,#5F89E6)] w-full '>
         <div className='flex flex-col p-4 gap-1'>
-            <p className='text-sm font-semibold text-white'>Preview of Gadwalify</p>
-            <p className='text-sm font-semibold text-white'>Sign up to get unlimited songs and podcasts with occasional ads. No credit card needed.</p>
+            <p className='text-xs md:text-sm font-semibold text-white'>Preview of Gadwalify</p>
+            <p className='text-xs md:text-sm font-semibold text-white'>Sign up to get unlimited songs and podcasts with occasional ads. No credit card needed.</p>
 
         </div>
         {
           user ?(''): (<div className='w-100 flex justify-end px-5'>
-            <button onClick={handleSignup}  className='px-7 text-black py-3 bg-white rounded-4xl text-16px transform hover:scale-110 transition duration-300 font-bold'>Sign up free</button>
+            <button onClick={handleSignup}  className='px-5 md:px-7 text-black py-3 bg-white rounded-4xl text-16px transform hover:scale-110 transition duration-300 font-bold'>Sign up free</button>
 
         </div>)
         }
