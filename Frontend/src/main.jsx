1d917d9da722045d94b56c174b95587c6036148a
AmandeepRawat <Ar464163@gmail.com>
2026-05-22 10:56:39 +0530

    PWA implement

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -9,3 +9,12 @@ createRoot(document.getElementById('root')).render(
   // </StrictMode>,
   <App/>
 )
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("PWA Ready"))
+      .catch((err) => console.log(err));
+  });
+}
